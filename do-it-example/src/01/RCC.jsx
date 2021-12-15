adf0a8ab1870f5d946824e75348ffefbea824454
jun <hojuneee@naver.com>
2021-12-15 13:05:38 +0900

    12/15 오전수업

---
+++ do-it-example/src/01/RCC.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class RCC extends Component {
+  render() {
+    return (
+      <div>
+      Hello World!
+      
+      </div>
+    );
+  }
+}
+
+export default RCC;
